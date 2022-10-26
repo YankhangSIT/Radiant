be755132a371a118da3eaf60539cae6cda27280e
DonovanS-Prog <s.seah@digipen.edu>
2022-10-26 23:11:42 +0800

    mainmenu
    Update on mainmenu splashscreen

--- src/mainmenu.c
+++ src/mainmenu.c
@@ -15,13 +15,25 @@
 int panelDisplay = 0;
 CP_Image gunPlayer;
 CP_Image swordPlayer;
+CP_Image digipen;
 
+float TimeElapsed;
 extern int playerNum = 0;
 void Main_Menu_Init()
 {
 	//Set window width and height to variables
 	float wWidth = CP_System_GetWindowWidth();
 	float wHeight = CP_System_GetWindowHeight();
+
+	//Set TimeElapsed to float 0
+	//Load your image
+	//Position your image to the corner of top left
+	//Set the size according to your image size.
+	TimeElapsed = 0.0f;
+	digipen = CP_Image_Load("./Assets/DigiPen_BLACK.png");
+	CP_Settings_ImageMode(CP_POSITION_CORNER);
+	CP_Settings_ImageWrapMode(CP_IMAGE_WRAP_CLAMP);
+	CP_System_SetWindowSize(wWidth * 2.5, wWidth * 2);
 
 	//set window size and center it
 	CP_System_SetWindowSize(wWidth * 2.5, wWidth * 2);
@@ -46,26 +58,44 @@ void Main_Menu_Update()
 	float wWidth = CP_System_GetWindowWidth();
 	float wHeight = CP_System_GetWindowHeight();
 
-	//Play Button
+
+	//Buffer time for splashscreen image & cursor creation
+	TimeElapsed += CP_System_GetDt();
+	CP_Settings_Fill(CP_Color_Create(255, 0, 0, 255));
 	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
-	CP_Settings_Fill(CP_Color_Create(0, 255, 0, 255));
-	CP_Graphics_DrawRect(wWidth / 2.0f, wHeight / 2.0f - 100, 180, 80);
-	CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
-	CP_Font_DrawText("Play", wWidth / 2.0f, wHeight / 2.0f - 100);
+	CP_Graphics_DrawCircle(CP_Input_GetMouseX(), CP_Input_GetMouseY(), 20.0f);
 
-	//Settings Button
-	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
-	CP_Settings_Fill(CP_Color_Create(0, 255, 0, 255));
-	CP_Graphics_DrawRect(wWidth / 2.0f, wHeight / 2.0f + 50, 180, 80);
-	CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
-	CP_Font_DrawText("Settings", wWidth / 2.0f, wHeight / 2.0f - 50);
+	float alphatime = 2;
+	if (TimeElapsed >= alphatime)
+	{
+		TimeElapsed = 0;
+	}
+	float resulttime = (TimeElapsed / alphatime) * 255;
+	CP_Image_Draw(digipen, 0, 0, CP_Image_GetWidth(digipen), CP_Image_GetHeight(digipen), (int)resulttime);
+	if (CP_Input_KeyDown(KEY_ENTER))
+	{
+		CP_Engine_Terminate();
 
-	//Exit Button
-	CP_Settings_Fill(CP_Color_Create(0, 255, 0, 255));
-	CP_Graphics_DrawRect(wWidth / 2.0f, wHeight / 2.0f + 100, 180, 80);
-	CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
-	CP_Font_DrawText("Exit", wWidth / 2.0f, wHeight / 2.0f + 50);
+		//Play Button
+		CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
+		CP_Settings_Fill(CP_Color_Create(0, 255, 0, 255));
+		CP_Graphics_DrawRect(wWidth / 2.0f, wHeight / 2.0f - 100, 180, 80);
+		CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+		CP_Font_DrawText("Play", wWidth / 2.0f, wHeight / 2.0f - 100);
 
+		//Settings Button
+		CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
+		CP_Settings_Fill(CP_Color_Create(0, 255, 0, 255));
+		CP_Graphics_DrawRect(wWidth / 2.0f, wHeight / 2.0f + 50, 180, 80);
+		CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+		CP_Font_DrawText("Settings", wWidth / 2.0f, wHeight / 2.0f - 50);
+
+		//Exit Button
+		CP_Settings_Fill(CP_Color_Create(0, 255, 0, 255));
+		CP_Graphics_DrawRect(wWidth / 2.0f, wHeight / 2.0f + 100, 180, 80);
+		CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
+		CP_Font_DrawText("Exit", wWidth / 2.0f, wHeight / 2.0f + 50);
+	}
 	//If click "Play" Button
 	if (CP_Input_MouseClicked()) {
 		CP_Vector mouseClickPos = CP_Vector_Set(CP_Input_GetMouseX(), CP_Input_GetMouseY());
@@ -108,8 +138,6 @@ void Main_Menu_Update()
 		CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
 		CP_Settings_Fill(CP_Color_Create(255, 255, 255, 255));
 		CP_Graphics_DrawRect(wWidth / 2.0f, wHeight / 2.0f - 100, 500, 500);
-		
-
 		
 		CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
 		CP_Font_DrawText("Choose your character", wWidth / 2.0f, wHeight / 2.0f - 300);
