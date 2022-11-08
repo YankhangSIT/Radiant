51ba9e9b40087dd7ae8e87912a30eeda18ce3ae2
jingsong <jingsong.wei@digipen.edu>
2022-11-08 19:08:23 +0800

    clean up

--- src/map.c
+++ src/map.c
@@ -6,27 +6,6 @@
 #include "level1.h"
 #include "map.h"
 
-Triangle SetTriangle(float x1, float y1, float x2, float y2, float x3, float y3, float degree)
-{
-	Triangle tri;
-	tri.x1 = x1;
-	tri.y1 = y1;
-	tri.x2 = x2;
-	tri.y2 = y2;
-	tri.x3 = x3;
-	tri.y3 = y3;
-	tri.degrees = degree;
-	return tri;
-}
-Circle SetCircle(float x, float y, float diameter, int clicked)
-{
-	Circle cir;
-	cir.x = x;
-	cir.y = y;
-	cir.diameter = diameter;
-	cir.clicked = clicked;
-	return cir;
-}
 Rect SetRect_(float x, float y, float width, float height)
 {
 	Rect rec;
@@ -36,17 +15,6 @@ Rect SetRect_(float x, float y, float width, float height)
 	rec.height = height;
 	return rec;
 }
-// typedef struct Resolution
-//{
-//	int width;
-//	int height;
-// }Resolution;
-// static Resolution SetResolution(int width, int height) {
-//	Resolution res;
-//	res.width = width;
-//	res.height = height;
-//	return res;
-// }Resolution windowResolution;
 
 CP_Vector checkMapCollision(CP_Vector charPosition, float minX, float maxX, float minY, float maxY)
 {
