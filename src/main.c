6a23a6c1ae88bf33640ef60cbc832a2f63d2e0b3
DonovanS-Prog <s.seah@digipen.edu>
2022-11-17 00:55:50 +0800

    Victory page creation (updated)

--- src/main.c
+++ src/main.c
@@ -23,9 +23,9 @@
 
 int main(void)
 {
-	CP_Engine_SetNextGameState(win_init, win_update, win_exit);
+	/*CP_Engine_SetNextGameState(win_init, win_update, win_exit);*/
 
-	/*CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);*/
+	CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
 	CP_Engine_Run();
 	return 0;
 }
