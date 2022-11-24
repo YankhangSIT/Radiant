350d21dfa3f7b9a2b205f73f3f2b6c23812fc954
DonovanS-Prog <s.seah@digipen.edu>
2022-11-25 01:05:12 +0800

    update main.c

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@ int main(void)
 {
 
 	
-	CP_Engine_SetNextGameState(Credits_4_Init, Credits_4_Update, Credits_4_Exit);
+	CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
 	//CP_Engine_SetNextGameState(leaderboard_init, leaderboard_update, leaderboard_exit);
 	CP_Engine_Run();
 	return 0;
