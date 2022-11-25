35a535b244bce9bee89ab5737877c70780892d75
DonovanS-Prog <s.seah@digipen.edu>
2022-11-25 15:24:40 +0800

    Leaderboard completion

--- src/main.c
+++ src/main.c
@@ -29,8 +29,8 @@ int main(void)
 {
 
 	
-	CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
-	//CP_Engine_SetNextGameState(leaderboard_init, leaderboard_update, leaderboard_exit);
+	//CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
+	CP_Engine_SetNextGameState(leaderboard_init, leaderboard_update, leaderboard_exit);
 	CP_Engine_Run();
 	return 0;
 }
